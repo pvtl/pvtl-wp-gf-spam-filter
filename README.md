8260cb9885f6427c91d9fa9979666493946decbd
Erin W <53369160+erinwelch@users.noreply.github.com>
2024-03-01 10:28:02 +1100

    Files init

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# pvtl-wp-gf-spam-filter
+# WP Gravity Forms Spam Filter
+
+Places spam filters into Wordpress to only allow Australian mobile and landline numbers (04, 02, 03, 07, 08)
+
+## Installation into a Bedrock site
+
+#### Install
+
+```
+# 1. Get it ready (to use a repo outside of packagist)
+composer config repositories.wp-gf-spam-filter git https://github.com/pvtl/wp-gf-spam-filter
+
+# 2. Install the Plugin
+composer require pvtl/wp-gf-spam-filter
+```
+
+#### Activate / Configure
+
+- Activate the plugin
+- When creating the gravity form, ensure that the phone number format is set to Australia, not international.
+
+![alt text](image.png)
---
+++ pvtl-gf-spam-filter.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Gravity Forms Spam Filter
+Plugin URI: https://www.pivotalagency.com.au/
+Description: Places spam filters into Wordpress to only allow Australian mobile and landline numbers (04, 02, 03, 07, 08)
+Author: Pivotal Agency
+Author URI: https://www.pivotalagency.com.au/
+Text Domain: pvtl-gf-spam-filter
+License: GPL3+
+*/
+
+// Gravity Forms - Phone REGEX AU Filter
+add_filter( 'gform_phone_formats', 'au_phone_format' );
+function au_phone_format( $phone_formats ) {
+    $phone_formats['au'] = array(
+        'label'       => 'Australia',
+        'mask'        => '99 9999 9999',
+        'regex'       => '/^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$/',
+        'instruction' => 'Australian phone numbers.',
+    );
+ 
+    return $phone_formats;
+}
+
+// Gravity Forms Spam Filter - disallowing 555- numbers & numbers starting with "8"
+if (!empty($phone_filter)) {
+    if (
+        ( strlen($phone_filter) == 11 && substr($phone_filter, 0, 1) == '8' ) ||
+        strlen($phone_filter) <= 6 ||
+        substr($phone_filter, 0, 4 ) === '555-'
+    ) {
+        // Spam block code here
+    }
+}
