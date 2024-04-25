7e0d0f816527d46a2414ad7ff66d39e3742520fc
Erin W <53369160+erinwelch@users.noreply.github.com>
2024-04-26 08:01:12 +1000

    Add AU address filter

--- pvtl-gf-spam-filter.php
+++ pvtl-gf-spam-filter.php
@@ -183,3 +183,26 @@ function filter_gform_entry_is_spam_detectlanguage( $is_spam, $form, $entry ) {
  
     return true;
 }
+
+// Add Australian address filter
+add_filter( 'gform_address_types', 'australian_address_type' );
+function australian_address_type( $address_types ) {
+    $address_types['australia'] = array(
+        'label'       => 'Australian',
+        'country'     => 'Australia',
+        'zip_label'   => 'Postcode',
+        'state_label' => 'State',
+        'states'      => array(
+            'ACT' => 'Australian Capital Territory',
+            'NT'  => 'Northern Territory',
+            'NSW' => 'New South Wales',
+            'QLD' => 'Queensland',
+            'SA'  => 'South Australia',
+            'TAS' => 'Tasmania',
+            'VIC' => 'Victoria',
+            'WA'  => 'Western Australia',
+        )
+    );
+ 
+    return $address_types;
+}
